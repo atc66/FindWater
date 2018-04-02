d173b1c1dbf4a9d3aa1ea61f04118d57066f6ea0
aciukurescu <andrewciukurescu@gmail.com>
2018-04-02 15:25:02 -0400

    dynamic nav bar for when you log in vs logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	helper_method :logged_in?,
+
+	def logged_in?
+		current_user != nil
+	end
+
+  def application
+  	@location = Location.new
+  end
+
+	private
+	def location_params
+    	params.require(:location).permit(:title, :lat, :lng)
+  	end
+
 end
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -10,6 +10,9 @@ class MapsController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
 	private
 	def location_params
     	params.require(:location).permit(:title, :lat, :lng)
