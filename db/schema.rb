ccd3ae8e967eb9958e039cdcf0b94f0d5f41dff3
aciukurescu <andrewciukurescu@gmail.com>
2018-04-04 17:53:47 -0400

    user can delete locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403005753) do
+ActiveRecord::Schema.define(version: 20180404210128) do
 
   create_table "locations", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180403005753) do
     t.float "lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "maps", force: :cascade do |t|
